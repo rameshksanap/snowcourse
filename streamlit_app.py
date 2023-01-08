f3d59883d960f32baa1173477191be35edc5ab07
rameshksanap <122223111+rameshksanap@users.noreply.github.com>
2023-01-08 17:51:13 +0100

    fourth change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Meal')
 streamlit.text('🐔 Rich and Curry')
 streamlit.text('🍞 Vadapav')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
