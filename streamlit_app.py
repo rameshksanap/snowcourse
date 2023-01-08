1f8a82805cf66db492b35f07c80de50c99a26a2d
rameshksanap <122223111+rameshksanap@users.noreply.github.com>
2023-01-08 17:49:12 +0100

    third change

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Meal')
-streamlit.text('Rich and Curry')
-streamlit.text('Vadapav')
+streamlit.text('🥣 Omega 3 and Blueberry Meal')
+streamlit.text('🐔 Rich and Curry')
+streamlit.text('🍞 Vadapav')
